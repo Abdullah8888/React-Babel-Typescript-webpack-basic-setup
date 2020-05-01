04b539ced7042807ca1546e53a1354a3e09e39a6
Jimoh Babatunde <jimoh.abdullahi@konga.com>
2020-05-01 01:54:46 +0100

    Initial Commit

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../styles/App.scss';
+
+class App extends React.PureComponent {
+	render() {
+		return (
+            <header className="App-header">
+				<h1>Asalam alaykum!</h1>
+            </header>
+			
+		);
+	}
+}
+
+export default App;
